6a5f0fe914b579e3611aa8ce4c391d8db0715fbf
Jose Miguel Garcia <jmgbenayas@gmail.com>
2018-04-12 20:48:07 +0200

    Finalizado el parseo y regex getNumber, error en mergeSort.

--- src/mergeSort/Main.java
+++ src/mergeSort/Main.java
@@ -1,10 +1,15 @@
 package mergeSort;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public void mergeSort(int[] list,int inicio,int fin){
+    public static void mergeSort(int[] list,int inicio,int fin){
         if (inicio == fin){
             return;
         }else{
@@ -14,7 +19,7 @@ public class Main {
             merge(list,inicio,medio+1,fin);
         }
     }
-    public void merge(int[] list, int inicio, int medio, int fin) {
+    public static void merge(int[] list, int inicio, int medio, int fin) {
         int[] izquierda = new int[medio - inicio + 2];
 
         for (int i = inicio; i <= medio; i++) {
@@ -40,16 +45,55 @@ public class Main {
             }
         }
     }
-    public static void main(String[] args) throws IOException {
-        String line = null;
-        try (Scanner scanner = new Scanner(new FileReader("resources/contestants.txt"))) {
-            line = scanner.nextLine();
-            int total = Integer.parseInt(line);
-            int[] list = new int[total];
-            while(scanner.hasNextLine()){
-                line = scanner.nextLine();
-                System.out.println(line);
+
+    public static int getNumber(String line){
+        String REGEX = "(\\w+) (\\d): (\\d+)";
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(line);
+        if(matcher.matches()){
+            return Integer.parseInt(matcher.group(matcher.groupCount()));
+        }
+        return -1;
+    }
+
+    public static void generateFile(int[][] list) throws IOException {
+        try(FileWriter writer = new FileWriter("output.txt")) {
+            for (int[] contestant : list) {
+                writer.write("Contestant "+contestant[0]+": "+contestant[1]);
             }
         }
+    }
+
+    public static int[] parser(String file) throws FileNotFoundException {
+        int[] list;
+        int total;
+        String line;
+        System.out.println(System.currentTimeMillis());
+        try (Scanner scanner = new Scanner(new FileReader(file))) {
+            line = scanner.nextLine();
+            total = Integer.parseInt(line);
+            list = new int[total];
+            int index = -1;
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                index = index + 1;
+                list[index] = getNumber(line);
+            }
+            System.out.println("Fin parseo");
+            System.out.println(System.currentTimeMillis()/1000);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String file = "resources/contestants.txt";
+        int[] list = null;
+        try {
+            list = parser(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        mergeSort(list,0,list.length);
+        System.out.println("FINAL");
     }
 }
